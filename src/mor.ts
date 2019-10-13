55d28fdf28cc71d43050510041e9346efd064544
sisisin <azsisisin@gmail.com>
2019-10-14 02:42:18 +0900

    update

--- src/mor.ts
+++ src/mor.ts
@@ -1,11 +1,29 @@
 import { Project } from 'ts-morph';
 import path from 'path';
+import fs from 'fs-extra';
 
-async function main() {
-  const p = new Project({ tsConfigFilePath: path.join(process.cwd(), 'fixtures/tsconfig.json') });
+async function prepare() {
+  await fs.remove('sandbox');
+  await fs.copy('fixtures', 'sandbox');
+}
+async function proc() {
+  const p = new Project({ tsConfigFilePath: path.join(process.cwd(), 'sandbox/tsconfig.json') });
   const t = p.getSourceFile('mod.ts');
   await t.moveImmediately('mod1.ts');
+
+  // const interfaces = t.getInterfaces();
+  // p.createSourceFile('interface.ts', interfaces.map(i => i.print()).join('\n'));
+
+  // interfaces.forEach(i => {
+  //   i.remove();
+  // });
+  // t.fixMissingImports();
+
   await p.save();
+}
+async function main() {
+  await prepare();
+  await proc();
 }
 
 main().catch(console.error);
